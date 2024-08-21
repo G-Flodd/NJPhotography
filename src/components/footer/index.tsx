b7014f0a1041a5760dc082d163d28f4643654b66
G-Flodd <georgedodd@hotmail.co.uk>
2024-08-21 19:07:59 +0100

    Building

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { AiFillInstagram } from "react-icons/ai";
+import { FaEtsy } from "react-icons/fa";
+
+export default function Footer() {
+    return (
+
+        <>
+            <section className="bg-[#94847c] shadow dark:bg-gray-800 mt-0">
+                <div className="container flex items-center justify-center p-6 mx-auto text-[#fefae0] capitalize ">
+                    <a href="https://instagram.com/pawtraitsbyabbey?igshid=YmMyMTA2M2Y=">
+                        <AiFillInstagram className="text-4xl mr-2" />
+                    </a>
+                    <a href="https://www.etsy.com/uk/shop/PawtraitsbyAbbey?ref=usf_2020">
+                        <FaEtsy className="text-3xl ml-2" />
+                    </a>
+
+
+                </div>
+            </section>
+        </>
+    )
+}
